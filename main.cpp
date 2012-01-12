c809342bef1b2faf62921254be8b9855b99c928e
Samuel Lidén Borell <samuel@slbdata.se>
2012-01-13 00:32:58 +0100

    Seed the random generator

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <stdexcept>
+#include <ctime>
+#include <cstdlib>
 
 #include "controller.h"
 
 int main() {
+    std::srand(std::time(NULL));
+    
     game::Controller controller;
     controller.run_game();
 
